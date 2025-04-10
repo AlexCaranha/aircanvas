289b64f063789352b0921b1ea85fd167b5085183
d4min <danishamin.rb@gmail.com>
2025-04-10 16:21:24 +0100

    indents

--- src/drawing.py
+++ src/drawing.py
@@ -26,55 +26,55 @@ class DrawingCanvas:
             "white": (255, 255, 255)
         }
 
-        def start_drawing(self, point):
-            self.drawing = True
-            self.start_point = point
+    def start_drawing(self, point):
+        self.drawing = True
+        self.start_point = point
 
-        def stop_drawing(self):
-            self.drawing = False
-            self.start_point = None
+    def stop_drawing(self):
+        self.drawing = False
+        self.start_point = None
 
-            if np.any(self.temp_canvas):
-                self.canvas = cv2.add(self.canvas, self.temp_canvas)
-                self.temp_canvas.fill(0)
-
-        def draw(self, point):
-            if not self.drawing:
-                return
-            
-            if self.current_tool == "pen":
-                cv2.line(self.canvas, self.start_point, point, self.colour, self.thickness)
-                self.start_point = point
-
-            elif self.current_tool == "eraser":
-                cv2.line(self.canvas, self.start_point, point,
-                        (0, 0, 0), self.eraser_thickness)
-                self.start_point = point
-
-            elif self.current_tool in ["rectangle", "circle"]:
-                self.temp_canvas.fill(0)
-
-                if self.current_tool == "rectangle":
-                    cv2.rectangle(self.temp_canvas, self.start_point, point, self.color, self.thickness)
-
-                else:
-                    center = self.start_point
-                    radius = int(((point[0] - center[0]) ** 2 + 
-                                (point[1] - center[1]) ** 2) ** 0.5)
-                    cv2.circle(self.temp_canvas, center, radius,
-                            self.color, self.thickness)
-                    
-        def clear(self):
-            self.canvas.fill(0)
+        if np.any(self.temp_canvas):
+            self.canvas = cv2.add(self.canvas, self.temp_canvas)
             self.temp_canvas.fill(0)
 
-        def set_colour(self, colour_name):
-            if colour_name in self.colours:
-                self.colour = self.colours[colour_name]
+    def draw(self, point):
+        if not self.drawing:
+            return
+        
+        if self.current_tool == "pen":
+            cv2.line(self.canvas, self.start_point, point, self.colour, self.thickness)
+            self.start_point = point
 
-        def set_tool(self, tool_name):
-            if tool_name in ["pen", "eraser", "rectangle", "circle"]:
-                self.current_tool = tool_name
+        elif self.current_tool == "eraser":
+            cv2.line(self.canvas, self.start_point, point,
+                    (0, 0, 0), self.eraser_thickness)
+            self.start_point = point
 
-        def get_display(self):
-            return cv2.add(self.canvas, self.temp_canvas)
+        elif self.current_tool in ["rectangle", "circle"]:
+            self.temp_canvas.fill(0)
+
+            if self.current_tool == "rectangle":
+                cv2.rectangle(self.temp_canvas, self.start_point, point, self.color, self.thickness)
+
+            else:
+                center = self.start_point
+                radius = int(((point[0] - center[0]) ** 2 + 
+                            (point[1] - center[1]) ** 2) ** 0.5)
+                cv2.circle(self.temp_canvas, center, radius,
+                        self.color, self.thickness)
+                
+    def clear(self):
+        self.canvas.fill(0)
+        self.temp_canvas.fill(0)
+
+    def set_colour(self, colour_name):
+        if colour_name in self.colours:
+            self.colour = self.colours[colour_name]
+
+    def set_tool(self, tool_name):
+        if tool_name in ["pen", "eraser", "rectangle", "circle"]:
+            self.current_tool = tool_name
+
+    def get_display(self):
+        return cv2.add(self.canvas, self.temp_canvas)
